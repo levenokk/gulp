d273bbcf7f8188714bdaad0f8032f4c5f343a3ed
levenokk <levenokk@yandex.ru>
2020-02-12 23:29:36 +0300

    Менеджер обновлений

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,43 @@
 let gulp = require('gulp'), //Сам gulp D
-    sass = require('gulp-sass'), // Подключаем препроцессор, если нужно изменить формат на saas, просто закоментируй таск scss и убери коментарии из таска ниже  
-    browserSync = require('browser-sync'), // Подключаем библиотеку для трансляции сайта и перезагрузки страницы после изменения любого из её файлов 
-    uglify = require('gulp-uglify'), // Подключаем библиотеку для сжатия js файлов
-    concat = require('gulp-concat'), // Подключаем библиотеку для объединения файлов
-    rename = require('gulp-rename'), // Подключаем библиотеку для переименования файлов
-    del = require('del'), // Подключаем библиотеку для удаления файлов
-    imagemin = require('gulp-imagemin'), // Подключаем библиотеку для работы с изображениями
-    pngquant = require('imagemin-pngquant'),// Подключаем библиотеку для работы с png
-    cache = require('gulp-cache'); // Подключаем библиотеку кеширования
+  sass = require('gulp-sass'), // Подключаем препроцессор, если нужно изменить формат на saas, просто закоментируй таск scss и убери коментарии из таска ниже  
+  browserSync = require('browser-sync'), // Подключаем библиотеку для трансляции сайта и перезагрузки страницы после изменения любого из её файлов 
+  uglify = require('gulp-uglify'), // Подключаем библиотеку для сжатия js файлов
+  concat = require('gulp-concat'), // Подключаем библиотеку для объединения файлов
+  rename = require('gulp-rename'), // Подключаем библиотеку для переименования файлов
+  del = require('del'), // Подключаем библиотеку для удаления файлов
+  imagemin = require('gulp-imagemin'), // Подключаем библиотеку для работы с изображениями
+  pngquant = require('imagemin-pngquant'),// Подключаем библиотеку для работы с png
+  cache = require('gulp-cache'), // Подключаем библиотеку кеширования
+  fetch = require('node-fetch'),
+  build = 1;
 
 
+console.log('Gulp by levenokk version 1.0, build 1\n');
+async function checkVer() {
+  let check = await fetch('https://raw.githubusercontent.com/levenokk/gulp/dev/version.json');
+  let json = await check.json();
+  let result = json.build;
 
-gulp.task('clean', async function(){
+  if (build < result) {
+    console.log(`Опа, вышла новая версия ${json.ver}, build ${json.build}, вы можете её скачать по этой сылке:\n` + json.link + '\n');
+  }
+
+  setTimeout(() => {
+    checkVer();
+  }, 30000);
+  
+}
+
+gulp.task('clean', async function () {
   del.sync('dist')
 })
 
-gulp.task('scss', function(){
+gulp.task('scss', function () {
   return gulp.src('app/scss/**/*.scss')
-    .pipe(sass({outputStyle: 'compressed'}))
-    .pipe(rename({suffix: '.min'}))
+    .pipe(sass({ outputStyle: 'compressed' }))
+    .pipe(rename({ suffix: '.min' }))
     .pipe(gulp.dest('app/css'))
-    .pipe(browserSync.reload({stream: true}))
+    .pipe(browserSync.reload({ stream: true }))
 });
 
 //     Вот с этого таска(Убери всё //, и сотри это предложения потом))
@@ -33,34 +50,34 @@ gulp.task('scss', function(){
 // });
 
 
-gulp.task('css', function(){
+gulp.task('css', function () {
   return gulp.src([
     'node_modules/normalize.css/normalize.css',
     'node_modules/slick-carousel/slick/slick.css'
   ])
     .pipe(concat('_libs.scss'))
     .pipe(gulp.dest('app/scss'))
-    .pipe(browserSync.reload({stream: true}))
+    .pipe(browserSync.reload({ stream: true }))
 });
 
-gulp.task('html', function(){
+gulp.task('html', function () {
   return gulp.src('app/*.html')
-  .pipe(browserSync.reload({stream: true}))
+    .pipe(browserSync.reload({ stream: true }))
 });
 
-gulp.task('script', function(){
+gulp.task('script', function () {
   return gulp.src('app/js/*.js')
-  .pipe(browserSync.reload({stream: true}))
+    .pipe(browserSync.reload({ stream: true }))
 });
 
-gulp.task('js', function(){
+gulp.task('js', function () {
   return gulp.src([
     'node_modules/slick-carousel/slick/slick.js',
   ])
     .pipe(concat('libs.min.js'))
     .pipe(uglify())
     .pipe(gulp.dest('app/js'))
-    .pipe(browserSync.reload({stream: true}))
+    .pipe(browserSync.reload({ stream: true }))
 });
 
 // gulp.task('fonts', function(){
@@ -69,26 +86,26 @@ gulp.task('js', function(){
 //     .pipe(gulp.dest('app/scss'))
 // })
 
-gulp.task('browser-sync', function() {
+gulp.task('browser-sync', function () {
   browserSync.init({
-      server: {
-          baseDir: "app/"
-      }
+    server: {
+      baseDir: "app/"
+    }
   });
 });
 
-gulp.task('img', function() {
+gulp.task('img', function () {
   return gulp.src('app/img/**/*') // Берем все изображения из app
-      .pipe(cache(imagemin({  // Сжимаем их с наилучшими настройками с учетом кеширования
-          interlaced: true,
-          progressive: true,
-          svgoPlugins: [{removeViewBox: false}],
-          use: [pngquant()]
-      })))
-      .pipe(gulp.dest('dist/img')); // Выгружаем на продакшен
+    .pipe(cache(imagemin({  // Сжимаем их с наилучшими настройками с учетом кеширования
+      interlaced: true,
+      progressive: true,
+      svgoPlugins: [{ removeViewBox: false }],
+      use: [pngquant()]
+    })))
+    .pipe(gulp.dest('dist/img')); // Выгружаем на продакшен
 });
 
-gulp.task('export', async function(){
+gulp.task('export', async function () {
   let buildHtml = gulp.src('app/**/*.html')
     .pipe(gulp.dest('dist'));
 
@@ -97,7 +114,7 @@ gulp.task('export', async function(){
 
   let BuildJs = gulp.src('app/js/**/*.js')
     .pipe(gulp.dest('dist/js'));
-    
+
   let BuildFonts = gulp.src('app/fonts/**/*.*')
     .pipe(gulp.dest('dist/fonts'));
 
@@ -106,10 +123,10 @@ gulp.task('export', async function(){
 });
 
 gulp.task('clear', function (callback) {
-	return cache.clearAll();
+  return cache.clearAll();
 })
 
-gulp.task('watch', function(){
+gulp.task('watch', function () {
   gulp.watch('app/scss/**/*.scss', gulp.parallel('scss'));
   gulp.watch('app/*.html', gulp.parallel('html'))
   gulp.watch('app/js/*.js', gulp.parallel('script'))
@@ -120,3 +137,8 @@ gulp.task('build', gulp.series('clean', 'export', 'clear', 'img'));
 gulp.task('bootstrap', gulp.parallel('scss', 'browser-sync', 'watch'));
 
 gulp.task('default', gulp.parallel('css', 'scss', 'js', 'browser-sync', 'watch'));
+
+setTimeout(() => {
+  checkVer();
+}, 10000);
+
