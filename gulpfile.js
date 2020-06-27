27b05236905039cc33d8484bbd4bff760c912c9c
levenokk <levenokk@gmail.com>
2020-06-27 19:39:57 +0300

    first commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,25 @@
-let gulp = require('gulp'), //Сам gulp D
-  sass = require('gulp-sass'), // Подключаем препроцессор, если нужно изменить формат на saas, просто закоментируй таск scss и убери коментарии из таска ниже  
-  browserSync = require('browser-sync'), // Подключаем библиотеку для трансляции сайта и перезагрузки страницы после изменения любого из её файлов 
-  uglify = require('gulp-uglify'), // Подключаем библиотеку для сжатия js файлов
-  concat = require('gulp-concat'), // Подключаем библиотеку для объединения файлов
-  rename = require('gulp-rename'), // Подключаем библиотеку для переименования файлов
-  del = require('del'), // Подключаем библиотеку для удаления файлов
-  imagemin = require('gulp-imagemin'), // Подключаем библиотеку для работы с изображениями
-  pngquant = require('imagemin-pngquant'),// Подключаем библиотеку для работы с png
-  cache = require('gulp-cache'), // Подключаем библиотеку кеширования
-  fetch = require('node-fetch'),
-  build = 3;
+let gulp = require("gulp"), //Сам gulp D
+    sass = require("gulp-sass"), // Подключаем препроцессор, если нужно изменить формат на saas, просто закоментируй таск scss и убери коментарии из таска ниже
+    browserSync = require("browser-sync"), // Подключаем библиотеку для трансляции сайта и перезагрузки страницы после изменения любого из её файлов
+    uglify = require("gulp-uglify"), // Подключаем библиотеку для сжатия js файлов
+    concat = require("gulp-concat"), // Подключаем библиотеку для объединения файлов
+    rename = require("gulp-rename"), // Подключаем библиотеку для переименования файлов
+    del = require("del"), // Подключаем библиотеку для удаления файлов
+    imagemin = require("gulp-imagemin"), // Подключаем библиотеку для работы с изображениями
+    pngquant = require("imagemin-pngquant"), // Подключаем библиотеку для работы с png
+    cache = require("gulp-cache"); // Подключаем библиотеку кеширования
 
+gulp.task("clean", async function() {
+    del.sync("dist");
+});
 
-console.log('Gulp by levenokk version 1.1, build 2\n');
-async function checkVer() {
-  let check = await fetch('https://raw.githubusercontent.com/levenokk/gulp/dev/version.json');
-  let json = await check.json();
-  let result = json.build;
-
-  if (build < result) {
-    console.log(`Опа, вышла новая версия ${json.ver}, build ${json.build}, вы можете её скачать по этой сылке:\n` + json.link + '\n');
-  }
-
-  setTimeout(() => {
-    checkVer();
-  }, 30000);
-  
-}
-
-gulp.task('clean', async function () {
-  del.sync('dist')
-})
-
-gulp.task('scss', function () {
-  return gulp.src('app/scss/**/*.scss')
-    .pipe(sass({ outputStyle: 'compressed' }))
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(gulp.dest('app/css'))
-    .pipe(browserSync.reload({ stream: true }))
+gulp.task("scss", function() {
+    return gulp
+        .src("app/scss/**/*.scss")
+        .pipe(sass({ outputStyle: "compressed" }))
+        .pipe(rename({ suffix: ".min" }))
+        .pipe(gulp.dest("app/css"))
+        .pipe(browserSync.reload({ stream: true }));
 });
 
 //     Вот с этого таска(Убери всё //, и сотри это предложения потом))
@@ -49,35 +31,33 @@ gulp.task('scss', function () {
 //     .pipe(browserSync.reload({stream: true}))
 // });
 
-
-gulp.task('css', function () {
-  return gulp.src([
-    'node_modules/normalize.css/normalize.css',
-    'node_modules/slick-carousel/slick/slick.css'
-  ])
-    .pipe(concat('_libs.scss'))
-    .pipe(gulp.dest('app/scss'))
-    .pipe(browserSync.reload({ stream: true }))
+gulp.task("css", function() {
+    return gulp
+        .src([
+            "node_modules/normalize.css/normalize.css",
+            "node_modules/bootstrap/dist/css/bootstrap-grid.css",
+            "onepage-scroll/onepage-scroll.css",
+        ])
+        .pipe(concat("_libs.scss"))
+        .pipe(gulp.dest("app/scss"))
+        .pipe(browserSync.reload({ stream: true }));
 });
 
-gulp.task('html', function () {
-  return gulp.src('app/*.html')
-    .pipe(browserSync.reload({ stream: true }))
+gulp.task("html", function() {
+    return gulp.src("app/*.html").pipe(browserSync.reload({ stream: true }));
 });
 
-gulp.task('script', function () {
-  return gulp.src('app/js/*.js')
-    .pipe(browserSync.reload({ stream: true }))
+gulp.task("script", function() {
+    return gulp.src("app/js/*.js").pipe(browserSync.reload({ stream: true }));
 });
 
-gulp.task('js', function () {
-  return gulp.src([
-    'node_modules/slick-carousel/slick/slick.js',
-  ])
-    .pipe(concat('libs.min.js'))
-    .pipe(uglify())
-    .pipe(gulp.dest('app/js'))
-    .pipe(browserSync.reload({ stream: true }))
+gulp.task("js", function() {
+    return gulp
+        .src(["onepage-scroll/jquery.onepage-scroll.js"])
+        .pipe(concat("libs.min.js"))
+        .pipe(uglify())
+        .pipe(gulp.dest("app/js"))
+        .pipe(browserSync.reload({ stream: true }));
 });
 
 // gulp.task('fonts', function(){
@@ -86,59 +66,59 @@ gulp.task('js', function () {
 //     .pipe(gulp.dest('app/scss'))
 // })
 
-gulp.task('browser-sync', function () {
-  browserSync.init({
-    server: {
-      baseDir: "app/"
-    }
-  });
+gulp.task("browser-sync", function() {
+    browserSync.init({
+        server: {
+            baseDir: "app/",
+        },
+    });
 });
 
-gulp.task('img', function () {
-  return gulp.src('app/img/**/*') // Берем все изображения из app
-    .pipe(cache(imagemin({  // Сжимаем их с наилучшими настройками с учетом кеширования
-      interlaced: true,
-      progressive: true,
-      svgoPlugins: [{ removeViewBox: false }],
-      use: [pngquant()]
-    })))
-    .pipe(gulp.dest('dist/img')); // Выгружаем на продакшен
+gulp.task("img", function() {
+    return gulp
+        .src("app/img/**/*") // Берем все изображения из app
+        .pipe(
+            cache(
+                imagemin({
+                    // Сжимаем их с наилучшими настройками с учетом кеширования
+                    interlaced: true,
+                    progressive: true,
+                    svgoPlugins: [{ removeViewBox: false }],
+                    use: [pngquant()],
+                })
+            )
+        )
+        .pipe(gulp.dest("dist/img")); // Выгружаем на продакшен
 });
 
-gulp.task('export', async function () {
-  let buildHtml = gulp.src('app/**/*.html')
-    .pipe(gulp.dest('dist'));
+gulp.task("export", async function() {
+    let buildHtml = gulp.src("app/**/*.html").pipe(gulp.dest("dist"));
 
-  let BuildCss = gulp.src('app/css/**/*.css')
-    .pipe(gulp.dest('dist/css'));
+    let BuildCss = gulp.src("app/css/**/*.css").pipe(gulp.dest("dist/css"));
 
-  let BuildJs = gulp.src('app/js/**/*.js')
-    .pipe(gulp.dest('dist/js'));
+    let BuildJs = gulp.src("app/js/**/*.js").pipe(gulp.dest("dist/js"));
 
-  let BuildFonts = gulp.src('app/fonts/**/*.*')
-    .pipe(gulp.dest('dist/fonts'));
+    let BuildFonts = gulp.src("app/fonts/**/*.*").pipe(gulp.dest("dist/fonts"));
 
-  // let BuildImg = gulp.src('app/img/**/*.*')
-  //   .pipe(gulp.dest('dist/img'));   
+    // let BuildImg = gulp.src('app/img/**/*.*')
+    //   .pipe(gulp.dest('dist/img'));
 });
 
-gulp.task('clear', function (callback) {
-  return cache.clearAll();
-})
-
-gulp.task('watch', function () {
-  gulp.watch('app/scss/**/*.scss', gulp.parallel('scss'));
-  gulp.watch('app/*.html', gulp.parallel('html'))
-  gulp.watch('app/js/*.js', gulp.parallel('script'))
+gulp.task("clear", function(callback) {
+    return cache.clearAll();
 });
 
-gulp.task('build', gulp.series('clean', 'export', 'clear', 'img'));
+gulp.task("watch", function() {
+    gulp.watch("app/scss/**/*.scss", gulp.parallel("scss"));
+    gulp.watch("app/*.html", gulp.parallel("html"));
+    gulp.watch("app/js/*.js", gulp.parallel("script"));
+});
 
-gulp.task('bootstrap', gulp.parallel('scss', 'browser-sync', 'watch'));
+gulp.task("build", gulp.series("clean", "export", "clear", "img"));
 
-gulp.task('default', gulp.parallel('css', 'scss', 'js', 'browser-sync', 'watch'));
+gulp.task("bootstrap", gulp.parallel("scss", "browser-sync", "watch"));
 
-setTimeout(() => {
-  checkVer();
-}, 10000);
-
+gulp.task(
+    "default",
+    gulp.parallel("css", "scss", "js", "browser-sync", "watch")
+);
